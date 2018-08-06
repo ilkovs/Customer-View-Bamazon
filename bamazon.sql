8cae03defcadefd292933bbe352d82568684ace0
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-08-05 23:20:28 -0400

    need to fix few bugs

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products(
-	item_id INTEGER NOT NULL AUTO_INCREMENT,
+	item_id INT AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,13 +6,97 @@ var connection = mysql.createConnection({
     host: "localHost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "root",
     database: "bamazon_db"
 });
 
 //connect to the mysql server and database
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("You are connected!");
+    console.log("\n--------------------------------------------------\n");
+    // console.log("You are connected!");
+
+    itemsForSale();
 });
+
+
+//list all items for sale from the DB
+function itemsForSale() {
+    connection.query(
+       "SELECT item_id, product_name, price FROM products",
+       function(err, result) {
+           if (err) throw err;
+           console.log(result);
+       }
+    );
+}
+
+//Promp the customer
+promptToPurchase();
+
+//create the Prompt Function
+function promptToPurchase() {
+inquirer
+    .prompt([
+        {
+            name: "item_id",
+            type: "input",
+            message: "Type the ID of the product that you would like to buy:"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many units of the item you would like to buy ?"
+        },
+
+    ]).then(function(input) {
+
+        var customerItem = input.item_id;
+        var quantity = input.quantity;
+
+        //check availability of the selected item
+        var checkDB = "SELECT * FROM products WHERE ?";
+
+        connection.query(checkDB, {item_id: customerItem}, function(err, data) {
+
+            if (err) throw err;
+
+            //specify item from 1-10
+            if (data.length === 0) {
+                console.log("You have to select item between 1-10. Try again.");
+                itemsForSale();
+
+            } else {
+
+                var productData = data[0];
+
+                // the requested quantity should be below the one available in the inventory
+                if (quantity <= productData.quantity) {
+                    
+                    console.log("The item is in stock. Processing your order!");
+
+                    // update the invetory after the order, subtract the ordered quantity from the inventory 
+                    var updateInventory = "UPDATE products SET quantity = " + (productData.quantity - quantity) + "WHERE item_id " + item;
+
+                    connection.query(updateInventory, function(err, data) {
+                        if (err) throw err;
+
+                        console.log("Your order has been received! Your total is $" + productData.price * quantity);
+                        console.log("\n------------------------------------\n");
+                        connection.end();
+
+                    })
+
+                } else {
+                    console.log("We are sorry, we don't have enough items in stock, please modify your order.");
+                    console.log("\n--------------------------------------\n");
+
+                    itemsForSale();
+                }
+            }
+        })
+    })
+}
+
+
 
